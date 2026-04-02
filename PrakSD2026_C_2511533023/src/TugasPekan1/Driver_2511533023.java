370e331043b8d84c93d279873669e2de76393503
QUlXOTE <soulland207@gmail.com>
2026-04-02 21:52:49 +0700

    TugasLaprakPekan1_2511533023

---
+++ PrakSD2026_C_2511533023/src/TugasPekan1/Driver_2511533023.java
@@ -0,0 +1,24 @@
+package TugasPekan1;
+
+public class Driver_2511533023 {
+    public static void main(String[] args) {
+
+        ADTMobil_2511533023 dataMobil = new ADTMobil_2511533023();
+
+        // contoh sesuai soal
+        ADTMobil_2511533023 m1 = new ADTMobil_2511533023("Avanza", 2020, 1300, 150000000, "Toyota");
+
+        // tambah mobil
+        dataMobil.tambahMobil(m1);
+
+        // tampilkan
+        System.out.println("Data Mobil:");
+        dataMobil.tampilkanMobil();
+
+        // hapus mobil
+        dataMobil.hapusMobil(0);
+
+        System.out.println("\nSetelah dihapus:");
+        dataMobil.tampilkanMobil();
+    }
+}
